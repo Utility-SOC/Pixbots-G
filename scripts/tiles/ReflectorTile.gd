8be9486b741fdc0fefd18d5ddad783384d1712c9
Utility-SOC <utility@quokkaclock.cyou>
2026-06-13 04:10:28 -0700

    Fix integer overflow in total energy, add rotate support to ReflectorTile, hide misleading static paths on AmplifierTile

--- scripts/tiles/ReflectorTile.gd
+++ scripts/tiles/ReflectorTile.gd
@@ -14,3 +14,11 @@ func process_energy(packet: EnergyPacket, entry_direction: int, grid: Node = nul
 	var new_dir = get_exit_direction(entry_direction)
 	packet.direction = new_dir
 	return [packet]
+
+func rotate(clockwise: bool = true):
+	if clockwise:
+		rotation_steps = (rotation_steps + 1) % 6
+	else:
+		rotation_steps = (rotation_steps - 1) % 6
+		if rotation_steps < 0:
+			rotation_steps += 6
